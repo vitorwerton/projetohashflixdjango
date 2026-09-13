a22cf076fb82f9aa20af782ad888c8456801af69
Vitor Cruz Werton Sales <vitorwerton@hotmail.com>
2026-09-13 19:03:15 -0300

    update app.py

--- filme/apps.py
+++ filme/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 
 
 class FilmeConfig(AppConfig):
@@ -6,13 +7,27 @@ class FilmeConfig(AppConfig):
     name = 'filme'
 
     def ready(self):
-        from .models import Usuario
-        import os
+        post_migrate.connect(
+            criar_usuario_admin,
+            sender=self
+        )
 
-        email = os.getenv('EMAIL_ADMIN')
-        senha = os.getenv('SENHA_ADMIN')
 
-        usuarios = Usuario.objects.filter(email=email)
-        if not usuarios:
-            Usuario.objects.create_superuser(username="admin", email=email, password=senha,
-                                             is_active=True, is_staff=True)
+def criar_usuario_admin(sender, **kwargs):
+    from .models import Usuario
+    import os
+
+    email = os.getenv('EMAIL_ADMIN')
+    senha = os.getenv('SENHA_ADMIN')
+
+    if not email or not senha:
+        return
+
+    if not Usuario.objects.filter(email=email).exists():
+        Usuario.objects.create_superuser(
+            username="admin",
+            email=email,
+            password=senha,
+            is_active=True,
+            is_staff=True
+        )
